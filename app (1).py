15eaa4926cd90437a8b29aeabe47c72fed6db0d1
aan-a <gautamaarna777@gmail.com>
2025-07-10 19:07:39 +0530

    Add files via upload

---
+++ app (1).py
@@ -0,0 +1,37 @@
+
+import streamlit as st
+import torch
+import torchvision.transforms as transforms
+from torchvision import models
+from PIL import Image
+import torch.nn as nn
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = models.resnet18(weights=None)
+model.fc = nn.Linear(model.fc.in_features, 10)
+model.load_state_dict(torch.load('resnet_cifar10.pth', map_location=device))
+model.eval().to(device)
+
+classes = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck']
+
+st.title("CIFAR-10 Image Classifier 🧠📸")
+uploaded_file = st.file_uploader("Upload a 32x32 CIFAR-like image (jpg/png)...", type=['jpg', 'png'])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file).convert("RGB")
+    st.image(image, caption='Uploaded Image', use_column_width=True)
+
+    transform = transforms.Compose([
+        transforms.Resize((32, 32)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
+    ])
+    input_tensor = transform(image).unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        outputs = model(input_tensor)
+        _, predicted = torch.max(outputs, 1)
+        predicted_class = classes[predicted.item()]
+
+    st.markdown(f"### 🧠 Prediction: **{predicted_class.upper()}**")
